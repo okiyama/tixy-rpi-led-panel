9ec9e894556aa803add223f4e8798e7d0c13c735
Julian Jocque <lamabuny@gmail.com>
2020-11-25 21:19:01 +0000

    Working with status callback function and too slow

--- src/_config.js
+++ src/_config.js
@@ -1,4 +1,16 @@
-import { GpioMapping, LedMatrix, } from 'rpi-led-matrix';
-export const matrixOptions = Object.assign(Object.assign({}, LedMatrix.defaultMatrixOptions()), { rows: 32, cols: 32, chainLength: 1, hardwareMapping: GpioMapping.Regular, parallel: 1 });
-export const runtimeOptions = Object.assign(Object.assign({}, LedMatrix.defaultRuntimeOptions()), { gpioSlowdown: 0 });
+import {
+    GpioMapping,
+    LedMatrix,
+} from 'rpi-led-matrix';
+
+export const matrixOptions = Object.assign(Object.assign({}, LedMatrix.defaultMatrixOptions()), {
+    rows: 32,
+    cols: 32,
+    chainLength: 1,
+    hardwareMapping: GpioMapping.Regular
+});
+
+export const runtimeOptions = Object.assign(Object.assign({}, LedMatrix.defaultRuntimeOptions()), {
+    gpioSlowdown: 0
+});
 
--- src/index.js
+++ src/index.js
@@ -1,30 +1,48 @@
 import { LedMatrix } from 'rpi-led-matrix';
 import { matrixOptions, runtimeOptions } from './_config.js';
+
+const callback = (t, i, x, y) => Math.sin(y/8 + t);
+
 class Pulser {
-    constructor(x, y, f) {
+    constructor(x, y) {
         this.x = x;
         this.y = y;
-        this.f = f;
+        this.value = null;
     }
-    nextColor(t) {
-        /** You could easily work position-dependent logic into this expression */
-        const brightness = 0xFF & Math.max(0, (255 * (Math.sin(this.x * t / 1000))));
-        return (brightness << 16) | (brightness << 8) | brightness;
+    calc(t, i) {
+        this.value = callback(t, i, this.x, this.y);
+    }
+    nextColor() {
+        if(this.value < 0) {
+            return 0xFF0000;
+        } else {
+            return 0xFFFFFF;
+        }
+    }
+    brightness() {
+        return Math.abs(this.value) * 100;
     }
 }
 (async () => {
     try {
+        let index = 0;
         const matrix = new LedMatrix(matrixOptions, runtimeOptions);
         const pulsers = [];
         for (let x = 0; x < matrix.width(); x++) {
             for (let y = 0; y < matrix.height(); y++) {
-                pulsers.push(new Pulser(x, y, 5 * Math.random()));
+                pulsers.push(new Pulser(x, y));
             }
         }
         matrix.afterSync((mat, dt, t) => {
+            console.log(dt);
             pulsers.map(pulser => {
-                matrix.fgColor(pulser.nextColor(t)).setPixel(pulser.x, pulser.y);
+                pulser.calc(t/1000, index);
+                matrix
+                    .fgColor(pulser.nextColor())
+                    .brightness(pulser.brightness())
+                    .setPixel(pulser.x, pulser.y);
             });
+	    index++;
             setTimeout(() => matrix.sync(), 0);
         });
         matrix.sync();
